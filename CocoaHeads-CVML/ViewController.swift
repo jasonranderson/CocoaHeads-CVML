11367a52d4f2e2b560536d2c0ff558834b7a2db8
Jason R. Anderson <jason@kosoku.com>
2018-09-23 15:02:37 -0400

    Initial Commit

---
+++ CocoaHeads-CVML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoaHeads-CVML
+//
+//  Created by Jason Anderson on 9/23/18.
+//  Copyright © 2018 Kosoku Interactive, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
